615a043458d53ab16e58f79d4e49641d1d177ac5
eva040100 <k00267259@student.tus.ie>
2023-12-13 12:37:11 +0000

    fixed the dig script

--- Assets/Scripts/DigAction.cs
+++ Assets/Scripts/DigAction.cs
@@ -6,6 +6,7 @@ using UnityEngine.InputSystem;
 public class DigAction : MonoBehaviour
 {
     public Interaction dig;
+    public bool colliding;
     // Start is called before the first frame update
     void Start()
     {
@@ -14,10 +15,21 @@ public class DigAction : MonoBehaviour
 
     // Update is called once per frame
     void Update()
-    {
+    { if (colliding)
+        {
+            digging();
+        }  
         
     }
-    private void OnCollisionStay(Collision collision)
+    private void OnCollisionEnter(Collision collision)
+    {
+        colliding = true;
+    }
+    private void OnCollisionExit(Collision collision)
+    {
+        colliding = false;
+    }
+    public void digging()
     {
         if (UnityEngine.Input.GetButton("Dig") && Interaction.Instance.digs >= 1)
         {
